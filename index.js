ca662df70c09fa72507604609a9537ac700dc800
David Soto Sousa <sousavlc86@gmail.com>
2023-04-19 13:33:55 +0200

    feat: Estructura de la BBDD para levantarla

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+// require('dotenv').config();
+const db = require('./db');
+const app = express();
+const PORT = process.env.PORT || 4000;
+app.use(express.json());
+
+db.then(() => {
+    //Starting server
+    app.listen(PORT, () => console.log("Server on port " + PORT));
+})
+    .catch((err) => console.log(err.message));
