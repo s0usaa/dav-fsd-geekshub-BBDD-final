5c75b9d295a324ea2684d345ab1ce06d3355dc9e
David Soto Sousa <sousavlc86@gmail.com>
2023-04-20 10:39:09 +0200

    fix: error en el archivo config.json

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// require('dotenv').config();
+require('dotenv').config();
 const db = require('./db');
 const app = express();
 const PORT = process.env.PORT || 4000;
