916278a6cfccb74789c47dc28a14657e4d41c6f8
David Soto Sousa <sousavlc86@gmail.com>
2023-04-20 12:34:35 +0200

    feat: Prueba del endpoint /welcome para probar la BBDD

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ require('dotenv').config();
 const db = require('./db');
 const app = express();
 const PORT = process.env.PORT || 4000;
+const router = require('./router');
 app.use(express.json());
+
+app.get('/welcome', (req, res)=>{
+    return res.send('Bienvenido a mi app')
+})
+
+app.use(router)
+
+
 
 db.then(() => {
     //Starting server
