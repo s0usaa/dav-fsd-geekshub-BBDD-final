ed77aae4277e31eff1e92a88b6e693a5eab93f9e
David Soto Sousa <sousavlc86@gmail.com>
2023-04-27 12:12:34 +0200

    feat: CRUD de pistas como Admin y creacion de entrenadores

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -1,5 +1,5 @@
 const adminControllers = {};
-const {User, Track} = require('../models');
+const {User, Track, Coach} = require('../models');
 
 //Ver todos los usuarios como Admin
 adminControllers.getAllUsers = async(req, res)=>{
@@ -21,7 +21,7 @@ adminControllers.getAllUsers = async(req, res)=>{
     }
 }
 
-//Ver usuarios por ID
+//Ver usuarios por ID como Admin
 adminControllers.getUserById = async(req, res)=>{
     try {
         const userId = req.params.id;
@@ -50,7 +50,7 @@ adminControllers.getUserById = async(req, res)=>{
     }
 }
 
-//Crear pistas
+//Crear pistas como Admin
 adminControllers.createTracks = async(req, res)=>{
     try {
         const {track_number, type} = req.body
@@ -74,4 +74,138 @@ adminControllers.createTracks = async(req, res)=>{
         });
     }
 }
+
+//Eliminar una pista como Admin
+adminControllers.deleteTracks = async(req, res)=>{
+    try {
+        const trackId = req.params.id;
+
+        const tracks = await Track.findOne({
+            where:{
+                id: trackId
+            }
+        });
+
+        if(!tracks){
+            return res.send('No existe esa pista');
+        }
+
+        const trackDelete = await Track.destroy({
+            where:{
+                id: trackId
+            }
+        });
+
+        return res.json({
+            success: true,
+            data: trackDelete
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: 'Error al borrar una pista',
+            error_message: error.message
+        });
+    }
+}
+
+//Modificar una pista como Admin
+adminControllers.updateTracks = async(req, res)=>{
+    try {
+        const {id, type} = req.body;
+
+        const tracks = await Track.findOne({
+            where:{
+                id:id
+            }
+        });
+
+        if(!tracks){
+            return res.send('No existe esa pista');
+        }
+
+        const trackUpdate = await Track.update({
+            id:id,
+            type: type
+        },
+        {where:{
+            id:id
+        }
+        });
+
+        if(!trackUpdate){
+            return res.send('Pista no modificada');
+        }
+
+        return res.send('Pista modificada');
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: 'Fallo al actualizar la pista',
+            error_message: error.message
+        });
+    }
+}
+
+//Ver pistas como Admin
+adminControllers.getTracks = async(req, res)=>{
+    try {
+        const getTracks = await Track.findAll({
+            attributes:{exclude: ['createdAt', 'updatedAt']}
+        });
+
+        return res.json({
+            success: true,
+            data: getTracks
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: 'Error al ver las pistas',
+            error_message: error.message
+        });
+    }
+}
+
+//Ver entrenadores como Admin
+adminControllers.getCoaches = async(req, res)=>{
+    try {
+        const getCoaches = await Coach.findAll({
+            attributes:{exclude: ['createdAt', 'updatedAt']}
+        });
+
+        return res.json({
+            success: true,
+            data: getCoaches
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: 'Error al ver los entrenadores',
+            error_message: error.message
+        });
+    }
+}
+
+//Crear un entrenador como Admin
+adminControllers.createCoaches = async(req,res)=>{
+    try {
+        const {especialidad} = req.body
+
+        const addCoach ={
+            especialidad: especialidad
+        }
+
+        const newTrack = await Coach.create(addCoach)
+
+        return res.json({
+            success: true,
+            data: newTrack
+        })
+    } catch (error) {
+        
+    }
+}
+
+
 module.exports = adminControllers;
--- views/adminRoutes.js
+++ views/adminRoutes.js
@@ -6,5 +6,10 @@ const isAdmin = require ('../middleware/isAdmin');
 router.get('/users', verifyToken, isAdmin, adminControllers.getAllUsers);
 router.get('/users/:id', verifyToken, isAdmin, adminControllers.getUserById);
 router.post('/tracks', verifyToken, isAdmin, adminControllers.createTracks);
+router.delete('/tracks/:id', verifyToken, isAdmin, adminControllers.deleteTracks);
+router.put('/tracks', verifyToken, isAdmin, adminControllers.updateTracks);
+router.get('/tracks', verifyToken, isAdmin, adminControllers.getTracks);
+router.get('/coaches', verifyToken, isAdmin, adminControllers.getCoaches);
+router.post('/coaches', verifyToken, isAdmin, adminControllers.createCoaches);
 
 module.exports = router;
